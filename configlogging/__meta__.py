c3a44df9f6c701ba6aa75f2ecc6a9c1a4dd9575d
Derrick Gilland <dgilland@gmail.com>
2014-12-16 11:06:40 -0500

    Add Tox testing command to setup.py.

--- configlogging/__meta__.py
+++ configlogging/__meta__.py
@@ -1,21 +1,13 @@
 """Project metadata
 """
 
-__all__ = [
-    '__title__',
-    '__summary__',
-    '__url__',
-    '__version__',
-    '__author__',
-    '__email__',
-    '__license__']
-
 __title__ = 'configlogging'
 __summary__ = 'Simple helper moudle for configuring Python logging'
-
 __url__ = 'https://github.com/dgilland/configlogging'
 
 __version__ = '0.1.1'
+
+__install_requires__ = ['PyYAML>=3.11']
 
 __author__ = 'Derrick Gilland'
 __email__ = 'dgilland@gmail.com'
--- setup.py
+++ setup.py
@@ -4,15 +4,46 @@ configlogging
 
 Simple helper for configuring Python's logging module.
 
+Project: https://github.com/dgilland/configlogging
+
 Documentation: https://github.com/dgilland/configlogging
 """
 
-from setuptools import setup
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 meta = {}
-with open('configlogging/__meta__.py') as fp:
-    exec(fp.read(), meta)
+exec(read('configlogging/__meta__.py'), meta)
+
+
+class Tox(TestCommand):
+    user_options = [
+        ('tox-args=', 'a', "Arguments to pass to tox")
+    ]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = '-c tox.ini'
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Import here because outside the eggs aren't loaded.
+        import tox
+        import shlex
+
+        errno = tox.cmdline(args=shlex.split(self.tox_args))
+        sys.exit(errno)
 
 
 setup(
@@ -23,11 +54,11 @@ setup(
     author=meta['__author__'],
     author_email=meta['__email__'],
     description=meta['__summary__'],
-    long_description=__doc__,
-    packages=['configlogging'],
-    install_requires=[
-        'PyYAML>=3.11'
-    ],
+    long_description=read('README.rst'),
+    packages=find_packages(exclude=['tests']),
+    install_requires=meta['__install_requires__'],
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
     test_suite='tests',
     keywords='logging config configuration',
     classifiers=[
@@ -35,12 +66,15 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Logging',
         'Topic :: System :: Systems Administration',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
