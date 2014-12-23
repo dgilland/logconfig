2dac8cbb354e46cef927096c04add1c46d229b49
Derrick Gilland <dgilland@gmail.com>
2014-12-23 10:00:58 -0500

    Add __all__ and alphabetize functions.

--- configlogging/__init__.py
+++ configlogging/__init__.py
@@ -13,9 +13,66 @@ import logging.config
 from ._compat import string_types
 
 
+__all__ = (
+    'ConfigLoggingException',
+    'from_autodetect'
+    'from_dict',
+    'from_env',
+    'from_file',
+    'from_filename',
+    'from_json',
+    'from_yaml'
+)
+
+
 class ConfigLoggingException(Exception):
     """Base exception for configlogging module."""
     pass
+
+
+def from_autodetect(obj):
+    """Dispatch logging configuration based on object type."""
+    if isinstance(obj, dict):
+        from_dict(obj)
+    elif isinstance(obj, string_types):
+        from_filename(obj)
+    else:
+        raise ConfigLoggingException(
+            ('Unable to autodetect object: {0}'.format(repr(obj))))
+
+
+def from_dict(dct):
+    """Configure logging module using dict object."""
+    logging.config.dictConfig(dct)
+
+
+def from_env(var):
+    """Dispatch logging configuration based on filename provided through
+    environment variable.
+    """
+    value = os.getenv(var)
+    from_filename(value)
+
+
+def from_file(filename, **kargs):
+    """Configure logging module using configparser-format file."""
+    logging.config.fileConfig(filename, **kargs)
+
+
+def from_filename(filename):
+    """Dispatch logging configuration based on filename."""
+    ext = os.path.splitext(filename)[1]
+
+    if ext in ('.json',):
+        from_json(filename)
+    elif ext in ('.yml', '.yaml'):
+        from_yaml(filename)
+    elif ext in ('.cfg', '.ini', '.conf', '.config'):
+        from_file(filename)
+    else:
+        raise ConfigLoggingException(('Unrecognized filename. '
+                               'Supported filename extensions: '
+                               'json, yml, yaml, cfg, ini, conf, config'))
 
 
 def from_json(filename):
@@ -32,48 +89,3 @@ def from_yaml(filename):
         config = yaml.load(fileobj)
 
     logging.config.dictConfig(config)
-
-
-def from_file(filename, **kargs):
-    """Configure logging module using configparser-format file."""
-    logging.config.fileConfig(filename, **kargs)
-
-
-def from_dict(dct):
-    """Configure logging module using dict object."""
-    logging.config.dictConfig(dct)
-
-
-def from_filename(filename):
-    """Dispatch logging configuration based on filename."""
-    ext = os.path.splitext(filename)[1]
-
-    if ext in ('.json',):
-        from_json(filename)
-    elif ext in ('.yml', '.yaml'):
-        from_yaml(filename)
-    elif ext in ('.cfg', '.ini', '.conf', '.config'):
-        from_file(filename)
-    else:
-        raise ConfigException(('Unrecognized filename. '
-                               'Supported filename extensions: '
-                               'json, yml, yaml, cfg, ini, conf, config'))
-
-
-def from_env(var):
-    """Dispatch logging configuration based on filename provided through
-    environment variable.
-    """
-    value = os.getenv(var)
-    from_filename(value)
-
-
-def from_autodetect(obj):
-    """Dispatch logging configuration based on object type."""
-    if isinstance(obj, dict):
-        from_dict(obj)
-    elif isinstance(obj, string_types):
-        from_filename(obj)
-    else:
-        raise ConfigException(('Unable to autodetect object: {0}'
-                               .format(repr(obj))))
