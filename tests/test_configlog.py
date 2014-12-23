77f49d0d5f3133e57523f2b16204e276f9de8606
Derrick Gilland <dgilland@gmail.com>
2014-12-23 10:22:10 -0500

    Rename test file and update renamed module references.

--- tests/test_configlog.py
+++ tests/test_configlog.py
@@ -3,9 +3,10 @@ import unittest
 import logging
 import os
 
-import configlogging
+import configlog
 
 from .logging_dict import logging_dict
+
 
 # Py3 compatibility
 if 'reload' not in dir(__builtins__):
@@ -17,8 +18,8 @@ logging_yaml = 'tests/logging.yml'
 logging_file = 'tests/logging.cfg'
 
 
-class TestConfiglogging(unittest.TestCase):
-    """Tests for configlogging module."""
+class TestConfiglog(unittest.TestCase):
+    """Tests for configlog module."""
 
     def setUp(self):
         """Set up logging module to initial state."""
@@ -38,73 +39,73 @@ class TestConfiglogging(unittest.TestCase):
 
     def test_from_json(self):
         """Test that from_json loads config object."""
-        configlogging.from_json(logging_json)
+        configlog.from_json(logging_json)
         self.assert_config()
 
     def test_from_yaml(self):
         """Test that from_yaml loads config object."""
-        configlogging.from_yaml(logging_yaml)
+        configlog.from_yaml(logging_yaml)
         self.assert_config()
 
     def test_from_file(self):
         """Test that from_file loads config object."""
-        configlogging.from_file(logging_file)
+        configlog.from_file(logging_file)
         self.assert_config()
 
     def test_from_dict(self):
         """Test that from_dict loads config object."""
-        configlogging.from_dict(logging_dict)
+        configlog.from_dict(logging_dict)
         self.assert_config()
 
     def test_from_filename_json(self):
         """Test that from_filename loads config object from json."""
-        configlogging.from_filename(logging_json)
+        configlog.from_filename(logging_json)
         self.assert_config()
 
     def test_from_filename_yaml(self):
         """Test that from_filename loads config object from yaml."""
-        configlogging.from_filename(logging_yaml)
+        configlog.from_filename(logging_yaml)
         self.assert_config()
 
     def test_from_filename_file(self):
         """Test that from_filename loads config object from file."""
-        configlogging.from_filename(logging_file)
+        configlog.from_filename(logging_file)
         self.assert_config()
 
     def test_from_filename_exception(self):
         """Test that from_autodetect() throws exception on invalid object."""
-        self.assertRaises(configlogging.ConfigException,
-                          configlogging.from_filename,
+        self.assertRaises(configlog.ConfigLogException,
+                          configlog.from_filename,
                           'invalid')
 
     def test_from_autodetect_dict(self):
         """Test that from_autodetect() loads config object from dict."""
-        configlogging.from_autodetect(logging_dict)
+        configlog.from_autodetect(logging_dict)
         self.assert_config()
 
     def test_from_autodetect_json(self):
         """Test that from_autodetect() loads config object from json."""
-        configlogging.from_autodetect(logging_json)
+        configlog.from_autodetect(logging_json)
         self.assert_config()
 
     def test_from_autodetect_yaml(self):
         """Test that from_autodetect() loads config object from yaml."""
-        configlogging.from_autodetect(logging_yaml)
+        configlog.from_autodetect(logging_yaml)
         self.assert_config()
 
     def test_from_autodetect_file(self):
         """Test that from_autodetect() loads config object from file."""
-        configlogging.from_autodetect(logging_file)
+        configlog.from_autodetect(logging_file)
         self.assert_config()
 
     def test_from_autodetect_exception(self):
         """Test that from_autodetect() throws exception on invalid object."""
-        self.assertRaises(configlogging.ConfigException,
-                          configlogging.from_autodetect,
+        self.assertRaises(configlog.ConfigLogException,
+                          configlog.from_autodetect,
                           'invalid')
 
-        self.assertRaises(configlogging.ConfigException,
-                          configlogging.from_autodetect,
+        self.assertRaises(configlog.ConfigLogException,
+                          configlog.from_autodetect,
                           [])
 
     def test_from_env(self):
@@ -113,6 +114,6 @@ class TestConfiglogging(unittest.TestCase):
         """
         var = 'CL_TESTING'
         os.environ[var] = logging_json
-        configlogging.from_env(var)
+        configlog.from_env(var)
         self.assert_config()
         del os.environ[var]
