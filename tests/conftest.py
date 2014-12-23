767c145e43daf7b4cea3996453fb3400dc0e4a04
Derrick Gilland <dgilland@gmail.com>
2014-12-23 16:26:58 -0500

    Add pytest setup/teardown functions.

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+
+import logging
+
+import pytest
+
+
+def pytest_runtest_setup(item):
+    """Global setup for tests in this directory."""
+    logger = logging.getLogger()
+    logger.setLevel(logging.NOTSET)
+
+
+def pytest_runtest_teardown(item, nextitem):
+    """Global teardown for tests in this directory."""
+    logger = logging.getLogger()
+    logging.shutdown()
+    del logger.handlers[:]
+    del logging._handlerList[:]
